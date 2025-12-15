eab3a91b156c3c6d6c3971ee0256b9c8bd06c5aa
Chris <chris@grace-tech.org>
2025-12-15 18:57:27 -0500

    Clean up unused files and update dropdown text
    - Remove unused one-time migration scripts
    - Remove test and debug files
    - Remove unused includes folder (replaced by js/components.js)
    - Update dropdown: 'Michael Quarto' to 'Michael Quarto (founder)'

--- js/components.js
+++ js/components.js
@@ -38,7 +38,7 @@ const navbar = `
                         role="menu"
                         aria-labelledby="about-button"
                     >
-                        <a href="${pathPrefix}about/michael-quarto.html" class="block px-4 py-2 text-gray-700 hover:bg-gray-100 michael-quarto-link" role="menuitem">Michael Quarto</a>
+                        <a href="${pathPrefix}about/michael-quarto.html" class="block px-4 py-2 text-gray-700 hover:bg-gray-100 michael-quarto-link" role="menuitem">Michael Quarto (founder)</a>
                         <a href="${pathPrefix}about/the-gym.html" class="block px-4 py-2 text-gray-700 hover:bg-gray-100 the-gym-link" role="menuitem">The Gym</a>
                     </div>
                 </div>
